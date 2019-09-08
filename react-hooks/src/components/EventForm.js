7652feecfb8af51d83a6d7cab201e14faac3579e
musashi0128 <k.miyamoto.0128@gmail.com>
2019-09-08 17:16:51 +0900

    add all logs delete

--- react-hooks/src/components/EventForm.js
+++ react-hooks/src/components/EventForm.js
@@ -46,6 +46,17 @@ const EventForm = () => {
     }
   }
 
+  const deleteAllOpeationLogs =  e => {
+    e.preventDefault()
+    const result = window.confirm('全ての操作ログを本当に削除しても良いですか？')
+    if( result) {
+      dispatch({
+        type: DELETE_ALL_OPREATION_LOGS,
+      })
+
+    }
+  }
+
   const unCreatable = title === '' || body === ''
 
   return (
@@ -64,6 +75,7 @@ const EventForm = () => {
 
         <button className="btn btn-primary" onClick={addEvent} disabled={unCreatable}>イベントを作成する</button>
         <button className="btn btn-danger" onClick={deleteAllEvents} disabled={state.events.length === 0}>全てのイベントを削除する</button>
+        <button className="btn btn-danger" onClick={deleteAllOpeationLogs} disabled={state.operationLogs.length ===0 }>全ての操作ログを削除する</button>
       </form>
     </>
   )
